a6295bde0ccd9998565a5b7c392095456e3846e1
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-27 16:06:37 +0200

    merge pagination branches

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js";
+import { createPagination } from "./components/NavPagination/NavPagination.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector('[data-js="search-bar-container"]',);
+const searchBarContainer = document.querySelector(
+    '[data-js="search-bar-container"]',
+);
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
+nextButton.addEventListener("click", () => {
+    if (page < maxPage) {
+        page++;
+        fetchCharacters();
+    }
+});
+
+prevButton.addEventListener("click", () => {
+    if (page > 1) {
+        page--;
+        fetchCharacters();
+    }
+});
 
 // States // REPLACE CONST WITH LET //
 let maxPage = 1;
